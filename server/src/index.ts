58d036f5614ca80305bc3397885c5bdd7e96180a
Anton <aanohin98@gmail.com>
2020-02-11 18:23:47 +0300

    0.1.1 - Added typeScript in project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 9000;
+
 
 app.get("/", (req, res) => res.send("Hello Again!"));
 app.listen(port);
