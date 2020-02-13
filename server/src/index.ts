dfa3a7f049418f1ec48f593a02d411a1d2bc6dda
Anton <aanohin98@gmail.com>
2020-02-13 21:09:51 +0300

    0.1.9 - establish env and temp

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,26 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { typeDefs, resolvers } from './graphql';
+require("dotenv").config();
 
-const app = express();
-const port = 9000;
-const server = new ApolloServer({ typeDefs, resolvers });
+import express, { Application } from "express";
+import { ApolloServer } from "apollo-server-express";
+import { connectDatabase } from "./database";
+import { typeDefs, resolvers } from "./graphql";
 
-server.applyMiddleware({ app, path: '/api' });
-app.listen(port);
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
 
-console.log(`[app] : http://localhost:${port}`);
+  server.applyMiddleware({ app, path: "/api" });
+  app.listen(process.env.PORT);
+
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
+};
+
+mount(express());
+
+// Note: You will need to introduce a .env file at the root of the project
+// that has the PORT, DB_USER, DB_USER_PASSWORD, and DB_CLUSTER environment variables defined.
+// Otherwise, the server will not be able to start and/or connect to the database
