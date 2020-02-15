5986c6419e1425ec43b923b80986727221adb9c0
Anton <aanohin98@gmail.com>
2020-02-15 20:46:51 +0300

    0.2.2

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Listings} from './sections';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Listings title="Tinyhouse Listings" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
